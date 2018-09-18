cf5fca38c75b543c07130e1ea55a23a644ca59c0
Mike Wong <wongmdesign@gmail.com>
2018-09-18 14:19:28 -0400

    completed

--- pwd.js
+++ pwd.js
@@ -1,12 +1,13 @@
-module.exports = () => {
+module.exports = (input) => {
   process.stdout.write('prompt > ');
+  if (input === 'pwd') {
+    process.stdin.on('data', () => {
 
-  process.stdin.on('data', () => {
+      const cmd = process.cwd()
+        .toString().trim();
 
-    const cmd = process.cwd()
-      .toString().trim();
-
-    process.stdout.write(cmd);
-    process.stdout.write('\nprompt > ');
-  })
+      process.stdout.write(cmd);
+      process.stdout.write('\nprompt > ');
+    })
+  }
 }
